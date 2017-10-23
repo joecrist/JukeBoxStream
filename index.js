27f1fb72b4736c78c06fe47c57043746b20d7ec9
Joe Crist <joseph.crist@ymail.com>
2017-10-23 08:06:13 -0400

    first commit for JukeBox Stream

---
+++ index.js
@@ -0,0 +1,14 @@
+//exploit for streaming, hidden in nontected folder.
+
+var play = playsong.
+var stop = stopsong.
+var next = nextsong.
+var previous = pervioussong. 
+var pause = pausesong.
+//var list = this is to create an array of songs once I import the list
+
+
+//I'm having  trouble creating the correct syntax
+
+
+
